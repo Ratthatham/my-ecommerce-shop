87c943a6d84e844626b26d6695fe7caf23c713b9
Ratthatham siridol <ratthathamsiridol@Ratthathams-Macbook-Air.local>
2022-08-06 22:12:13 +0700

    Adding component

--- src/App.js
+++ src/App.js
@@ -1,49 +1,40 @@
 import React from 'react';
-
+import CategoryItems from '/Users/ratthathamsiridol/Documents/Fullstack Dev/my-shop/src/component/CategoryItems.js';
 
 
 
 const App = () => {
   const catagories = [
     {
-      "id": 1,
-      "title": "hats",
-      "imageUrl": "https://i.ibb.co/cvpntL1/hats.png"
+      id: 1,
+      title: "hats",
+      imageUrl: "https://i.ibb.co/cvpntL1/hats.png"
     },
     {
-      "id": 2,
-      "title": "jackets",
-      "imageUrl": "https://i.ibb.co/px2tCc3/jackets.png"
+      id: 2,
+      title: "jackets",
+      imageUrl: "https://i.ibb.co/px2tCc3/jackets.png"
     },
     {
-      "id": 3,
-      "title": "sneakers",
-      "imageUrl": "https://i.ibb.co/0jqHpnp/sneakers.png"
+      id: 3,
+      title: "sneakers",
+      imageUrl: "https://i.ibb.co/0jqHpnp/sneakers.png"
     },
     {
-      "id": 4,
-      "title": "womens",
-      "imageUrl": "https://i.ibb.co/GCCdy8t/womens.png"
+      id: 4,
+      title: "womens",
+      imageUrl: "https://i.ibb.co/GCCdy8t/womens.png"
     },
     {
-      "id": 5,
-      "title": "mens",
-      "imageUrl": "https://i.ibb.co/R70vBrQ/men.png"
+      id: 5,
+      title: "mens",
+      imageUrl: "https://i.ibb.co/R70vBrQ/men.png"
     }
   ];
 
   return (
-    <div className="categories-container">
-      {catagories.map(({title, id})=>(
-        <div key = {id} className='catagory-container'>
-          <div className='background-image'/>
-          <div className="category-body-container">
-            <h1>{title}</h1>
-            <p>Shop now</p>
-          </div>
-        </div>
-      ))}
-    </div>
+    <CategoryItems catagories = {catagories}/>
   );
 }
+
 export default App;
